d9ef195d2fd19114631b53d4a5c6f34b1c0ff1a2
Geraldo <geraldovilela19@gmail.com>
2021-04-08 15:52:29 -0400

    First Crud

---
+++ 03_RestWithASPNETCore/01_RestWithASPNETCore/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_RestWithASPNETCore.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{sencodNumber}")]
+        public IActionResult Get(string firstNumber, string sencodNumber)
+        {
+            return BadRequest("Invalid Input");
+        }
+       
+    }
+}
