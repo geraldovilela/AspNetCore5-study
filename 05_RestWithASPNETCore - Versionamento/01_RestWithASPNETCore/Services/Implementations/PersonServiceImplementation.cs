75c345314aee669320a9d7d54bd2734935ad3e14
Geraldo <geraldovilela19@gmail.com>
2021-04-18 19:36:57 -0400

    adicionando versionamento a API;

---
+++ 05_RestWithASPNETCore - Versionamento/01_RestWithASPNETCore/Services/Implementations/PersonServiceImplementation.cs
@@ -0,0 +1,92 @@
+﻿using _01_RestWithASPNETCore.Models;
+using _01_RestWithASPNETCore.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01_RestWithASPNETCore.Services.Implementations
+{
+    public class PersonServiceImplementation : IPersonService
+    {
+        private AppDBContext _context;
+        public PersonServiceImplementation(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(id));
+            if (result != null)
+            {
+                try
+                {
+                    _context.Persons.Remove(result);
+                    _context.SaveChanges();
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        public List<Person> FindAll()
+        {
+
+            return _context.Persons.ToList();
+        }
+
+        public Person FindById(long Id)
+        {
+            return _context.Persons.Find(Id);
+        }
+
+        public Person Update(Person person)
+        {
+            if (!Exists(person.Id)) return new Person();
+
+            var result = _context.Persons.SingleOrDefault(p => p.Id.Equals(person.Id));
+
+            if (result != null)
+            {
+                try
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+            return result;
+        }
+
+        private bool Exists(long Id)
+        {
+            return _context.Persons.Any(p => p.Id.Equals(Id));
+
+        }
+    }
+
+}
---
+++ 05_RestWithASPNETCore - Versionamento/01_RestWithASPNETCore/Startup.cs
@@ -0,0 +1,63 @@
+using _01_RestWithASPNETCore.Models.Context;
+using _01_RestWithASPNETCore.Services;
+using _01_RestWithASPNETCore.Services.Implementations;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01_RestWithASPNETCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+            var connection = Configuration["ConnectionStrings:Default"];
+            services.AddDbContext<AppDBContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 21))));
+
+            services.AddApiVersioning();
+            
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
