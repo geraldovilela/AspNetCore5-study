d0c7175f33d242a494707be3e44f746ba0eaa6e3
Geraldo <geraldovilela19@gmail.com>
2021-04-18 19:20:55 -0400

    conectando ao BD utilizando DBContext e EF;

---
+++ 04_RestWithASPNETCore - DB Context/01_RestWithASPNETCore/Models/Context/AppDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace _01_RestWithASPNETCore.Models.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext()
+        {
+
+        }
+
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options)
+        {
+        }
+            public DbSet<Person> Persons { get; set; }
+    }
+}
---
+++ 04_RestWithASPNETCore - DB Context/01_RestWithASPNETCore/Services/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿using _01_RestWithASPNETCore.Models;
+using System;
+using System.Collections.Generic;
+
+namespace _01_RestWithASPNETCore.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+        Person FindById(long Id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+    }
+}
